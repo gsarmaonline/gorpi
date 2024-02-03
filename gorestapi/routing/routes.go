9474e096ee31f9ea94bb75a9d38cb0036d68896f
Gaurav Sarma <gauravsarma1992@gmail.com>
2024-02-04 01:23:54 +0530

    Added tests for routes

--- gorestapi/routing/routes.go
+++ gorestapi/routing/routes.go
@@ -1,6 +1,9 @@
 package routing
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -27,13 +30,13 @@ func NewRouteManager(apiEngine *gin.Engine) (rm *RouteManager, err error) {
 	rm = &RouteManager{
 		apiEngine: apiEngine,
 	}
-	rm.trie = NewTrie(rm.RootHandler())
+	rm.trie = NewTrie(rm.DefaultRootRoute())
 	return
 }
 
 func (rm *RouteManager) DefaultRootRoute() (route *Route) {
 	route = &Route{
-		RequestURI:    "/",
+		RequestURI:    "",
 		RequestMethod: "*",
 		Handler:       rm.RootHandler,
 	}
@@ -45,10 +48,28 @@ func (rm *RouteManager) RootHandler(c *gin.Context) {
 }
 
 func (rm *RouteManager) AddRoutes(route *Route) (err error) {
-	rm.findLongestPrefix(route)
+	if _, err = rm.trie.AddPath(route); err != nil {
+		return
+	}
 	return
 }
 
-func (rm *RouteManager) findLongestPrefix(route *Route) (nearestRoute *Route, err error) {
+func (rm *RouteManager) GetRoute(path string) (route *Route, err error) {
+	var (
+		pathNode *Node
+	)
+	if pathNode, err = rm.trie.GetNode(path); err != nil {
+		return
+	}
+	if pathNode.Route == nil {
+		err = errors.New("Route not found for path" + path)
+		return
+	}
+	route = pathNode.Route
+	return
+}
+
+func (route *Route) GetName() (name string) {
+	name = fmt.Sprintf("%s-%s", route.RequestMethod, route.RequestURI)
 	return
 }
--- gorestapi/routing/trie.go
+++ gorestapi/routing/trie.go
@@ -1,6 +1,8 @@
 package routing
 
 import (
+	"errors"
+	"fmt"
 	"strings"
 )
 
@@ -24,7 +26,7 @@ type (
 
 func NewTrie(rootRoute *Route) (tr *Trie) {
 	rootNode := &Node{
-		Name:      rootRoute.RequestURI,
+		Name:      rootRoute.GetName(),
 		prefixMap: make(map[string]*Node),
 		Route:     rootRoute,
 	}
@@ -34,40 +36,91 @@ func NewTrie(rootRoute *Route) (tr *Trie) {
 	return
 }
 
+func (tr *Trie) String() (content string) {
+	content = "hello"
+	tr.traverse(tr.rootNode, "", []string{})
+	return
+}
+
+func (tr *Trie) traverse(node *Node, path string, routesInPath []string) (updatedRoutesInPath []string) {
+	path += fmt.Sprintf("/%s", node.Name)
+	if node.Route != nil {
+		routesInPath = append(routesInPath, path)
+	}
+	for _, childNode := range node.prefixMap {
+		routesInPath = tr.traverse(childNode, path, routesInPath)
+	}
+	updatedRoutesInPath = routesInPath
+	return
+}
+
 func (tr *Trie) findLongestPrefix(spPath []string) (nearestNode *Node) {
 	currNode := tr.rootNode
 	for _, pathElem := range spPath {
 		pathNode := currNode.findOrCreateNode(pathElem)
 		currNode = pathNode
 	}
+	nearestNode = currNode
+	return
+}
+
+func (tr *Trie) translatePathFromStr(path string) (spPath []string) {
+	path = strings.Trim(path, "/")
+	spPath = strings.Split(path, "/")
 	return
 }
 
 func (tr *Trie) createPath(path string) (node *Node, err error) {
-	spPath := strings.Split(path, "/")
+	spPath := tr.translatePathFromStr(path)
 	node = tr.findLongestPrefix(spPath)
 	return
 }
 
 func (tr *Trie) AddPath(route *Route) (node *Node, err error) {
-	if node, err = tr.createPath(route.RequestURI); err != nil {
+	if node, err = tr.createPath(route.GetName()); err != nil {
 		return
 	}
 	node.Route = route
 	return
 }
 
+func (tr *Trie) GetNode(path string) (pathNode *Node, err error) {
+	spPath := tr.translatePathFromStr(path)
+	currNode := tr.rootNode
+	for _, pathElem := range spPath {
+		if pathNode, err = currNode.findNode(pathElem); err != nil {
+			return
+		}
+		currNode = pathNode
+	}
+	return
+}
+
+/*
+**
+====================================================
+Node methods
+**
+*/
 func (node *Node) findOrCreateNode(pathElem string) (pathNode *Node) {
-	isPresent := false
-	if pathNode, isPresent = node.prefixMap[pathElem]; !isPresent {
+	var (
+		err error
+	)
+	if pathNode, err = node.findNode(pathElem); err != nil {
 		pathNode = node.CreateChildNode(pathElem)
 	}
 	return
 }
 
+func (node *Node) findNode(pathElem string) (pathNode *Node, err error) {
+	isPresent := false
+	if pathNode, isPresent = node.prefixMap[pathElem]; !isPresent {
+		err = errors.New("Node not present at path: " + pathElem)
+	}
+	return
+}
+
 func (node *Node) CreateChildNode(pathElem string) (pathNode *Node) {
-	// TODO: pathElem should be checked for the presence of
-	// identifiers
 	pathNode = &Node{
 		Name:       pathElem,
 		prefixMap:  make(map[string]*Node),
